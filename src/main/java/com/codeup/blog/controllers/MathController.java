bd9d13c54fce8047cf477e680fe821851664444d
Emily Bowersox <emily.bowersox100@gmail.com>
2020-06-22 10:21:07 -0500

    math controller class for basic arithmetic

--- src/main/java/com/codeup/blog/controllers/MathController.java
+++ src/main/java/com/codeup/blog/controllers/MathController.java
@@ -1,4 +1,45 @@
 package com.codeup.blog.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class MathController {
+
+    //all using path variables!
+    //example:
+    //
+//    @GetMapping("/ads/{id}")
+//    @ResponseBody
+//    public String show(@PathVariable long id){
+//        return "show ad id: " + id;
+//    }
+
+    @GetMapping("/add/{num1}/and/{num2}")
+    @ResponseBody
+    public String add(@PathVariable long num1, @PathVariable long num2) {
+        return "show addition results: " + (num1 + num2);
+    }
+
+    @GetMapping("/subtract/{num1}/from/{num2}")
+    @ResponseBody
+    public String subtract(@PathVariable long num1, @PathVariable long num2) {
+        return "show subtraction results: " + (num1 - num2);
+    }
+
+    @GetMapping("/multiply/{num1}/and/{num2}")
+    @ResponseBody
+    public String multiply(@PathVariable long num1, @PathVariable long num2) {
+        return "show multiplication results: " + (num1 * num2);
+    }
+
+    @GetMapping("/divide/{num1}/by/{num2}")
+    @ResponseBody
+    public String divide(@PathVariable long num1, @PathVariable long num2) {
+        return "show division results: " + (num1 / num2);
+    }
+
+
 }

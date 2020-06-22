11f5f15f5a071cc8af261dc0b4710cf19558b2a3
Emily Bowersox <emily.bowersox100@gmail.com>
2020-06-22 10:37:52 -0500

    post controller

--- src/main/java/com/codeup/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -1,4 +1,41 @@
 package com.codeup.blog.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class PostController {
+
+    //method GET
+    @GetMapping("/posts")
+    @ResponseBody
+    public String getIndex() {
+        return "Here's the index page of posts.";
+    }
+
+    //method GET
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String getPost(@PathVariable int id) {
+        return "Here's the individual post you searched for: " + id;
+    }
+
+    //method GET
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String viewPostForm() {
+        return "Here's the form to create a new post.";
+    }
+
+    //method POST
+    //can't have two methods mapped to the same url??
+    @GetMapping("/posts/createpost")
+    @ResponseBody
+    public String createPost() {
+        return "Create a new post!";
+    }
+
+
 }

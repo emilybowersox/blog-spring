d99c3924b1784f054076b088405a7ea0bfd98f3a
Emily Bowersox <emily.bowersox100@gmail.com>
2020-06-22 10:03:43 -0500

    created method with GetMapping for the landing page

---
+++ src/main/java/com/codeup/blog/controllers/HomeController.java
@@ -0,0 +1,21 @@
+package com.codeup.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+//This class should have one method with a GetMapping for /.
+// It should return a string that says "This is the landing page!".
+
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String index(){
+        return "This is the landing page!";
+    }
+
+}

84653180eb4d726259f63769143b775cc5ce936f
Emily Bowersox <emily.bowersox100@gmail.com>
2020-06-23 13:49:48 -0500

    added id property to Post class

--- src/main/java/com/codeup/blog/models/Post.java
+++ src/main/java/com/codeup/blog/models/Post.java
@@ -1,27 +1,61 @@
 package com.codeup.blog.models;
 
+import javax.persistence.*;
+
 //This class will ultimately represent a POST record from our database.
 // The class should have private properties and getters and setters for a title and body.
+
+@Entity
+@Table(name = "posts")
 public class Post {
 
-private String title;
-private String body;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
 
-//getters
-public String getTitle(){
-    return title;
-}
-public String getBody(){
-    return body;
-}
+    @Column(nullable = false, length = 255)
+    private String title;
 
-//setters
-    public void setTitle(String title){
-    this.title = title;
+    @Column(nullable = false)
+    private String body;
+
+// empty constructor for the Spring framework to use (not for developers- for Spring)
+    public Post(){}
+
+    //constructor for "read"
+    public Post(long id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
     }
 
-    public void setBody(String body){
-    this.body = body;
+    //constructor for "insert"
+    public Post(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+
+    //getters
+    public long getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getBody() {
+        return body;
+    }
+
+    //setters
+    public void setId(long id){
+        this.id=id;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setBody(String body) {
+        this.body = body;
     }
 
 

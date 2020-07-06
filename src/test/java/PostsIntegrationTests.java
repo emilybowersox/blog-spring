f573a2c05a8118a1d019e79fd1505962ba16f663
Emily Bowersox <emily.bowersox100@gmail.com>
2020-07-06 12:25:55 -0500

    created and passed testCreatePost

--- src/test/java/PostsIntegrationTests.java
+++ src/test/java/PostsIntegrationTests.java
@@ -3,17 +3,20 @@ import com.codeup.blog.daos.PostsRepository;
 import com.codeup.blog.daos.UsersRepository;
 import com.codeup.blog.models.User;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.servlet.http.HttpSession;
 
+import static org.junit.Assert.assertNotNull;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
@@ -24,53 +27,75 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
     @AutoConfigureMockMvc
     public class PostsIntegrationTests {
 
-        private User testUser;
-        private HttpSession httpSession;
+    private User testUser;
+    private HttpSession httpSession;
 
-        @Autowired
-        private MockMvc mvc;
+    @Autowired
+    private MockMvc mvc;
 
-        @Autowired
-                //does this need to be usersDao
-        UsersRepository userDao;
+    @Autowired
+    //does this need to be usersDao
+            UsersRepository userDao;
 
-        @Autowired
-                //does this need to be postsDao
-        PostsRepository postDao;
+    @Autowired
+    //does this need to be postsDao
+            PostsRepository postDao;
 
-        @Autowired
-        private PasswordEncoder passwordEncoder;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-        @Before
-        public void setup() throws Exception {
+    @Before
+    public void setup() throws Exception {
 
-            testUser = userDao.findByUsername("testUser");
+        testUser = userDao.findByUsername("testUser");
 
-            // Creates the test user if not exists
-            if(testUser == null){
-                User newUser = new User();
-                newUser.setUsername("testUser");
-                newUser.setPassword(passwordEncoder.encode("pass"));
-                newUser.setEmail("testUser@codeup.com");
-                testUser = userDao.save(newUser);
-            }
-
-            // Throws a Post request to /login and expect a redirection to the Ads index page after being logged in
-            httpSession = this.mvc.perform(post("/login").with(csrf())
-                    .param("username", "testUser")
-                    .param("password", "pass"))
-                    .andExpect(status().is(HttpStatus.FOUND.value()))
-                    //changed ads to landing
-                    .andExpect(redirectedUrl("/landing"))
-                    .andReturn()
-                    .getRequest()
-                    .getSession();
+        // Creates the test user if not exists
+        if (testUser == null) {
+            User newUser = new User();
+            newUser.setUsername("testUser");
+            newUser.setPassword(passwordEncoder.encode("pass"));
+            newUser.setEmail("testUser@codeup.com");
+            testUser = userDao.save(newUser);
         }
+
+        // Throws a Post request to /login and expect a redirection to the Ads index page after being logged in
+        httpSession = this.mvc.perform(post("/login").with(csrf())
+                .param("username", "testUser")
+                .param("password", "pass"))
+                .andExpect(status().is(HttpStatus.FOUND.value()))
+                //changed ads to landing
+                .andExpect(redirectedUrl("/landing"))
+                .andReturn()
+                .getRequest()
+                .getSession();
+    }
+
+    @Test
+    public void contextLoads() {
+        // Sanity Test, just to make sure the MVC bean is working
+        assertNotNull(mvc);
     }
 
 
+    @Test
+    public void testIfUserSessionIsActive() throws Exception {
+        // It makes sure the returned session is not null
+        assertNotNull(httpSession);
+    }
 
+    @Test
+    public void testCreatePost() throws Exception {
+        // Makes a Post request to /ads/create and expect a redirection to the Ad
+        this.mvc.perform(
+                post("/posts/create").with(csrf())
+                        .session((MockHttpSession) httpSession)
+                        // Add all the required parameters to your request like this
+                        .param("title", "brand new mac")
+                        .param("body", "best buy sale"))
+                .andExpect(status().is3xxRedirection());
+    }
 
+}
 
 
 
